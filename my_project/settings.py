9414f6ee6542ba7f2662ef9a77014429abeb92b1
Monirul Islam <114785562+moniruldevelopers@users.noreply.github.com>
2025-07-26 23:55:42 +0600

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -167,8 +167,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' #my newline
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'monirul.developers@gmail.com'
-EMAIL_HOST_PASSWORD = 'yiei bjun ehnh skag'  
+EMAIL_HOST_USER = 'email your 
+EMAIL_HOST_PASSWORD = 'app password'  
 
 # Authentication
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # Set email verification to mandatory
